b2a0df702ca44184356e8579252422d5262083f8
Amel TRIKI <amel.triki.iset@gmail.com>
2024-11-23 17:01:19 +0100

    TP10 EX fini

--- src/app/components/public/deps/dep-list/dep-list.component.ts
+++ src/app/components/public/deps/dep-list/dep-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dep-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './dep-list.component.html',
   styleUrl: './dep-list.component.css'
 })
